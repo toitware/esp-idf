9db077d93a4ebd2c4255f5caa33f421aa18c9580
Anders Johnsen <anders@toitware.com>
2019-11-21 11:26:49 +0100

    Update to cmpctmalloc
    * Add cmpctmalloc to new release

---
+++ components/heap/third_party/dartino/cmpctmalloc.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2019 Toitware ApS. All rights reserved.
+
+#pragma once
+
+#include "../../include/multi_heap.h"
+
+multi_heap_handle_t cmpct_register_impl(void *start, size_t size);
+size_t cmpct_free_size_impl(multi_heap_handle_t heap);
+size_t cmpct_get_allocated_size_impl(multi_heap_handle_t heap, void *p);
+size_t cmpct_minimum_free_size_impl(multi_heap_handle_t heap);
+void cmpct_free_impl(multi_heap_handle_t heap, void *p);
+void cmpct_get_info_impl(multi_heap_handle_t heap, multi_heap_info_t *info);
+void *cmpct_malloc_impl(multi_heap_handle_t heap, size_t size);
+void *cmpct_realloc_impl(multi_heap_handle_t heap, void *p, size_t size);
