1837edd2e0bfd05e7e15506f0b013f4219344158
Erik Corry <erik@toitware.com>
2020-01-21 14:39:46 +0100

    Make malloc heap iteration more flexible

--- components/heap/third_party/dartino/cmpctmalloc.h
+++ components/heap/third_party/dartino/cmpctmalloc.h
@@ -12,4 +12,12 @@ void cmpct_free_impl(multi_heap_handle_t heap, void *p);
 void cmpct_get_info_impl(multi_heap_handle_t heap, multi_heap_info_t *info);
 void *cmpct_malloc_impl(multi_heap_handle_t heap, size_t size);
 void *cmpct_realloc_impl(multi_heap_handle_t heap, void *p, size_t size);
-void cmpct_iterate_tagged_memory_areas(multi_heap_handle_t heap, void *user_data, void *tag, tagged_memory_callback_t callback);
+void cmpct_iterate_tagged_memory_areas(multi_heap_handle_t heap, void *user_data, void *tag, tagged_memory_callback_t callback, int flags);
+
+// Flags for heap iteration.  Coordinate with constants in esp_heap_caps.h
+#define CMPCTMALLOC_ITERATE_UNLOCKED 1
+#define CMPCTMALLOC_ITERATE_ALL_ALLOCATIONS 2
+#define CMPCTMALLOC_ITERATE_UNUSED   4
+
+#define CMPCTMALLOC_ITERATE_TAG_FREE          (-1)  /// Memory is free and could be allocated.
+#define CMPCTMALLOC_ITERATE_TAG_HEAP_OVERHEAD (-2)  /// Memory is used by malloc for internal accounting etc.
