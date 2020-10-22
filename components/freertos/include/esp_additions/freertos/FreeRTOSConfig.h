45f230c2cad6062e15d688e8c8feb63921c3aa75
Erik Corry <erik@toitware.com>
2020-10-22 11:28:58 +0200

    Don't print function name on failed asserts.
    This just prints the file and line number, not the function
    name, if an assert fails.  This saves 1232 bytes on our build.

--- components/freertos/include/esp_additions/freertos/FreeRTOSConfig.h
+++ components/freertos/include/esp_additions/freertos/FreeRTOSConfig.h
@@ -42,8 +42,7 @@
 
 #if defined(CONFIG_FREERTOS_ASSERT_FAIL_PRINT_CONTINUE)
 #define configASSERT(a) if (unlikely(!(a))) {                               \
-        esp_rom_printf("%s:%d (%s)- assert failed!\n", __FILE__, __LINE__,  \
-                   __FUNCTION__);                                           \
+        ets_rom_printf("%s:%d assert failed!\n", __FILE__, __LINE__);       \
     }
 #elif defined(CONFIG_FREERTOS_ASSERT_FAIL_ABORT)
 #define configASSERT(a) assert(a)
