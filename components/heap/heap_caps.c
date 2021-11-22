a052d7cb247bb344397df23044496eeb0bcc1983
Erik Corry <erik@toitware.com>
2021-11-22 15:58:25 +0100

    Null protection on heap iteration (#46)

--- components/heap/heap_caps.c
+++ components/heap/heap_caps.c
@@ -525,7 +525,9 @@ void heap_caps_iterate_tagged_memory_areas(void *user_data, void *tag, tagged_me
 {
     heap_t *heap;
     SLIST_FOREACH(heap, &registered_heaps, next) {
-        multi_heap_iterate_tagged_memory_areas(heap->heap, user_data, tag, callback, flags);
+        if (heap->heap) {
+            multi_heap_iterate_tagged_memory_areas(heap->heap, user_data, tag, callback, flags);
+        }
     }
 }
 
