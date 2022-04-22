9fef785a56dfd1138e84218af97cf83bb9926570
Erik Corry <erik@toit.io>
2022-04-22 09:14:17 +0200

    Fix non-blocking output on UART (#58)

--- components/driver/uart.c
+++ components/driver/uart.c
@@ -1237,7 +1237,13 @@ static int uart_tx_all(uart_port_t uart_num, bool write_all, const char *src, si
         size_t max_size = xRingbufferGetMaxItemSize(p_uart_obj[uart_num]->tx_ring_buf);
         if(!write_all) {
             size_t max_free = xRingbufferGetCurFreeSize(p_uart_obj[uart_num]->tx_ring_buf);
+            if(max_free < sizeof(uart_tx_data_t)) {
+                xSemaphoreGive(p_uart_obj[uart_num]->tx_mux);
+                return 0;  // Not even enough space to send the control structure.
+            }
+            max_free -= sizeof(uart_tx_data_t);
             max_size = max_size < max_free ? max_size : max_free;
+            size = size > max_size ? max_size : size;
         }
         int offset = 0;
         uart_tx_data_t evt;
@@ -1255,9 +1261,6 @@ static int uart_tx_all(uart_port_t uart_num, bool write_all, const char *src, si
             size -= send_size;
             offset += send_size;
             uart_enable_tx_intr(uart_num, 1, UART_EMPTY_THRESH_DEFAULT);
-            if (!write_all) {
-                break;
-            }
         }
     } else {
         while (size) {
